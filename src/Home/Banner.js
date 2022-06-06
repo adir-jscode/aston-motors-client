2c7894231a7c0114f57a1fa8c64b10c4ab096c85
adir-jscode <adir.jscode@gmail.com>
2022-06-06 22:35:37 +0600

    aston

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <div class="hero-overlay bg-opacity-60"></div>
         <div class="hero-content text-center text-neutral-content">
           <div class="max-w-md">
-            <h1 class="mb-5 text-6xl text-white font-bold">Aston Motors</h1>
+            <h1 class="mb-5 text-6xl text-white font-bold">Aston</h1>
             <p class="text-white mb-5">OVER 60 YEARS OF BUSINESS THROUGHOUT</p>
           </div>
         </div>
